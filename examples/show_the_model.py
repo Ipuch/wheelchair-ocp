6044f4787723e258de136ab47679407061f2fe43
Samuel Hybois <samuel.hybois@universite-paris-saclay.fr>
2024-03-06 17:58:47 +0100

    FIX: reducing shoulder/elbow amplitude
    more physiological ranges for MWC propulsion

--- examples/show_the_model.py
+++ examples/show_the_model.py
@@ -1,11 +1,13 @@
 """
 This file is to display the human model into bioviz
 """
+
 import bioviz
 
-model_name = "wheelchair_model.bioMod"
+model_name = "models/wheelchair_model.bioMod"
 
-biorbd_viz = bioviz.Viz(model_name,
+biorbd_viz = bioviz.Viz(
+    model_name,
     show_gravity_vector=False,
     show_floor=False,
     show_local_ref_frame=True,
@@ -16,7 +18,7 @@ biorbd_viz = bioviz.Viz(model_name,
     show_segments_center_of_mass=True,
     mesh_opacity=0.5,
     background_color=(0.5, 0.5, 0.5),
-                         )
+)
 
 biorbd_viz.exec()
 print("Done")

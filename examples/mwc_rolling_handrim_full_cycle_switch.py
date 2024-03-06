34cbbc5797975de26954e710e14632b2b91bb91b
Samuel Hybois <samuel.hybois@universite-paris-saclay.fr>
2024-03-06 17:57:08 +0100

    FIX: declaration of single/multiple holonomic constraints

--- examples/mwc_rolling_handrim_full_cycle_switch.py
+++ examples/mwc_rolling_handrim_full_cycle_switch.py
@@ -56,11 +56,14 @@ def prepare_ocp(
 
     # --- Options --- #
     # BioModel path
-    bio_model = (BiorbdModelCustomHolonomic(biorbd_model_path), BiorbdModelCustomHolonomic(biorbd_model_path))
+    bio_model = (
+        BiorbdModelCustomHolonomic(biorbd_model_path, "push_phase"),
+        BiorbdModelCustomHolonomic(biorbd_model_path, "push_phase"),
+    )
 
     # Constraints
-    holonomic_constraints = HolonomicConstraintsList()
-    holonomic_constraints.add(
+    holonomic_constraints_push = HolonomicConstraintsList()
+    holonomic_constraints_push.add(
         key="rolling_joint_constraint",
         constraints_fcn=generate_rolling_joint_constraint,
         biorbd_model=bio_model[0],
@@ -69,7 +72,7 @@ def prepare_ocp(
         radius=0.35,
     )
 
-    holonomic_constraints.add(
+    holonomic_constraints_push.add(
         key="holonomic_constraint",
         constraints_fcn=generate_close_loop_constraint,
         biorbd_model=bio_model[0],
@@ -79,30 +82,31 @@ def prepare_ocp(
         local_frame_index=1,
     )
 
-    holonomic_constraints.add(
+    holonomic_constraints_recovery = HolonomicConstraintsList()
+    holonomic_constraints_recovery.add(
         key="rolling_joint_constraint",
         constraints_fcn=generate_rolling_joint_constraint,
         biorbd_model=bio_model[1],
         translation_joint_index=0,
         rotation_joint_index=1,
         radius=0.35,
-    )  # add 1 cas pour le calcul explicite si 1/2 contraintes sont activées !!
+    )
 
     bio_model[0].set_holonomic_configuration(
-        constraints_list=holonomic_constraints, independent_joint_index=[1], dependent_joint_index=[0, 2, 3]
+        constraints_list=holonomic_constraints_push, independent_joint_index=[1], dependent_joint_index=[0, 2, 3]
     )
 
     bio_model[1].set_holonomic_configuration(
-        constraints_list=holonomic_constraints, independent_joint_index=[1, 2, 3], dependent_joint_index=[0]
+        constraints_list=holonomic_constraints_recovery, independent_joint_index=[1, 2, 3], dependent_joint_index=[0]
     )
 
     # Objective functions
-    final_time = (1.5, 1.5)
+    final_time = (1.5, 2.2)
     objective_functions = ObjectiveList()
     objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", weight=100, multi_thread=False, phase=0)
-    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", weight=100, multi_thread=False, phase=1)
-    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_TIME, weight=1, min_bound=2, max_bound=2.3, phase=0)
-    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_TIME, weight=1, min_bound=2, max_bound=2.3, phase=1)
+    # objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", weight=100, multi_thread=False, phase=1)
+    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_TIME, weight=1, min_bound=1.3, max_bound=1.8, phase=0)
+    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_TIME, weight=1, min_bound=2, max_bound=4, phase=1)
 
     # Dynamics
     dynamics = DynamicsList()
@@ -126,9 +130,7 @@ def prepare_ocp(
     variable_bimapping_push.add("qdot", to_second=[None, 0, None, None], to_first=[1])
 
     variable_bimapping_recovery = BiMappingList()
-    variable_bimapping_recovery.add(
-        "q", to_second=[None, 0, 1, 2], to_first=[1, 2, 3]
-    )  # piloté en x pour la recovery ?
+    variable_bimapping_recovery.add("q", to_second=[None, 0, 1, 2], to_first=[1, 2, 3])
     variable_bimapping_recovery.add("qdot", to_second=[None, 0, 1, 2], to_first=[1, 2, 3])
 
     x_bounds = BoundsList()
@@ -139,26 +141,30 @@ def prepare_ocp(
     x_bounds.add("qdot_u", bounds=bio_model[1].bounds_from_ranges("qdot", mapping=variable_bimapping_recovery), phase=1)
 
     # Gérer les positions initiales/finales des DDL : wheel angle
-    x_bounds[0]["q_u"].min[0, 0] = 0.35
-    x_bounds[0]["q_u"].max[0, 0] = 0.35
-    x_bounds[0]["q_u"].min[0, -1] = -0.6
-    x_bounds[0]["q_u"].max[0, -1] = -0.6
+    x_bounds[0]["q_u"].min[0, 0] = 0.3
+    x_bounds[0]["q_u"].max[0, 0] = 0.5
+    x_bounds[0]["q_u"].min[0, -1] = -0.7
+    x_bounds[0]["q_u"].max[0, -1] = -0.4
 
-    x_bounds[1]["q_u"].min[0, 0] = -0.6
-    x_bounds[1]["q_u"].max[0, 0] = -0.6
-    x_bounds[1]["q_u"].min[0, -1] = 0.35
-    x_bounds[1]["q_u"].max[0, -1] = 0.35
+    # x_bounds[1]["q_u"].min[0, 0] = -0.6
+    # x_bounds[1]["q_u"].max[0, 0] = -0.6
+    # x_bounds[1]["q_u"].min[0, -1] = -3
+    # x_bounds[1]["q_u"].max[0, -1] = -2
+    # x_bounds[1]["q_u"].min[1, -1] = -2.15
+    # x_bounds[1]["q_u"].max[1, -1] = -2
+    # x_bounds[1]["q_u"].min[2, -1] = 0.5
+    # x_bounds[1]["q_u"].max[2, -1] = 0.7
 
     # Vitesses angulaires = 0
     x_bounds[0]["qdot_u"].min[:, 0] = 0
     x_bounds[0]["qdot_u"].max[:, 0] = 0
-    x_bounds[0]["qdot_u"].min[:, -1] = -0.1
-    x_bounds[0]["qdot_u"].max[:, -1] = 0.1
+    x_bounds[0]["qdot_u"].max[:, -1] = -1
+    x_bounds[0]["qdot_u"].min[:, -1] = -4
 
-    x_bounds[1]["qdot_u"].min[:, 0] = 0
-    x_bounds[1]["qdot_u"].max[:, 0] = 0
-    x_bounds[1]["qdot_u"].min[:, -1] = -0.1
-    x_bounds[1]["qdot_u"].max[:, -1] = 0.1
+    # x_bounds[1]["qdot_u"].min[:, 0] = 0
+    # x_bounds[1]["qdot_u"].max[:, 0] = 0
+    # x_bounds[1]["qdot_u"].min[:, -1] = -0.1
+    # x_bounds[1]["qdot_u"].max[:, -1] = 0.1
 
     # Initial guess
     x_init = InitialGuessList()
@@ -178,6 +184,7 @@ def prepare_ocp(
     u_bounds.add("tau", min_bound=[tau_min] * 2, max_bound=[tau_max] * 2, phase=1)
     u_bounds[0]["tau"].min[0, 0] = 0
     u_bounds[0]["tau"].max[0, 0] = 0
+    # u_bounds[1]["tau"].min[0, 0] = 0
 
     u_init = InitialGuessList()
     u_init.add("tau", initial_guess=[tau_init] * 2, phase=0)
@@ -221,46 +228,48 @@ def main():
     # ocp.add_plot_penalty(CostType.CONSTRAINTS)
     # --- Solve the program --- #
 
-    sol = ocp.solve(Solver.IPOPT(show_online_optim=False, show_options=dict(show_bounds=False), _max_iter=500))
+    sol = ocp.solve(Solver.IPOPT(show_online_optim=False, show_options=dict(show_bounds=False), _max_iter=0))
     states = sol.decision_states(to_merge=SolutionMerge.NODES)
     controls = sol.decision_controls(to_merge=SolutionMerge.NODES)
 
     # # --- Show results --- #
     q, qdot, qddot, lambdas = compute_all_states_from_indep_qu(sol, bio_model, variable_bimapping)
     #
+    q_cycle = np.hstack(q)
+
     import bioviz
 
     viz = bioviz.Viz(model_path)
-    viz.load_movement(q)
+    viz.load_movement(q_cycle)
     viz.exec()
 
-    import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
+    #
+    # time = sol.decision_time(to_merge=SolutionMerge.NODES)
+    # fig, axs = plt.subplots(1, 3)
+    #
+    # axs[0].plot(controls["tau"][0, :], label=r"$\tau_{shoulder}$")
+    # axs[0].plot(controls["tau"][1, :], label=r"$\tau_{elbow}$")
+    # axs[0].set_title("Controls of the OCP - actuated DoF")
+    # axs[0].set_ylabel("Torque (N.m)")
+    # axs[0].legend()
+    #
+    # axs[1].plot(time, qdot[2, :], "o", label=r"$\dot{theta}_{shoulder}$")
+    # axs[1].plot(time, qdot[-1, :], "o", label=r"$\dot{theta}_{elbow}$")
+    # axs[1].set_title("Controls of the OCP - actuated DoF")
+    # axs[1].set_xlabel("Time (s)")
+    # axs[1].set_ylabel("Torque (N.m)")
+    # axs[1].legend()
+    #
+    # axs[2].plot(time, lambdas[0, :], label="rolling")
+    # axs[2].plot(time, lambdas[1, :], label=r"$F_r$")
+    # axs[2].plot(time, lambdas[2, :], label=r"$F_{\theta}$")
+    # axs[2].set_title("Lagrange multipliers of the holonomic constraint")
+    # axs[2].set_xlabel("Time (s)")
+    # axs[2].set_ylabel("Lagrange multipliers (N)")
+    # axs[2].legend()
 
-    time = sol.decision_time(to_merge=SolutionMerge.NODES)
-    fig, axs = plt.subplots(1, 3)
-
-    axs[0].plot(controls["tau"][0, :], label=r"$\tau_{shoulder}$")
-    axs[0].plot(controls["tau"][1, :], label=r"$\tau_{elbow}$")
-    axs[0].set_title("Controls of the OCP - actuated DoF")
-    axs[0].set_ylabel("Torque (N.m)")
-    axs[0].legend()
-
-    axs[1].plot(time, qdot[2, :], "o", label=r"$\dot{theta}_{shoulder}$")
-    axs[1].plot(time, qdot[-1, :], "o", label=r"$\dot{theta}_{elbow}$")
-    axs[1].set_title("Controls of the OCP - actuated DoF")
-    axs[1].set_xlabel("Time (s)")
-    axs[1].set_ylabel("Torque (N.m)")
-    axs[1].legend()
-
-    axs[2].plot(time, lambdas[0, :], label="rolling")
-    axs[2].plot(time, lambdas[1, :], label=r"$F_r$")
-    axs[2].plot(time, lambdas[2, :], label=r"$F_{\theta}$")
-    axs[2].set_title("Lagrange multipliers of the holonomic constraint")
-    axs[2].set_xlabel("Time (s)")
-    axs[2].set_ylabel("Lagrange multipliers (N)")
-    axs[2].legend()
-
-    plt.show()
+    # plt.show()
 
 
 if __name__ == "__main__":
